b6812ab57c86bcbe6c3ec09312ad772838925b5a
Rukayat Adenike <ruqayyahibrahim983@gmail.com>
2023-06-25 20:16:58 +0100

    Added specifier_x_X function body

--- specifier_x_X.c
+++ specifier_x_X.c
@@ -1,12 +1,29 @@
+#include "main.h"
 
 /**
- * specifier_x_X - Function to handle the x and X specifier
+ * specifier_x - Function to handle the x specifier
  * @counter: Pointer to the character pointer
  * @value: void pointer to the value to print
  * Return: Void
  */
-void specifier_x_X(int *counter, void *value)
+void specifier_x(int *counter, void *value)
 {
-	(void)counter;
-	(void)value;
+	unsigned int n;
+
+	n = *((unsigned int *)value);
+	print_hex(n, 1, counter);
+}
+
+/**
+ * specifier_X - Function to handle the X specifier
+ * @counter: Pointer to the character pointer
+ * @value: void pointer to the value to print
+ * Return: Void
+ */
+void specifier_X(int *counter, void *value)
+{
+	unsigned int n;
+
+	n = *((unsigned int *)value);
+	print_hex(n, 0, counter);
 }
