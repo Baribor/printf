b2ba4bb30f461944cfbbaa3de728ffb10e816293
Rukayat Adenike <ruqayyahibrahim983@gmail.com>
2023-06-25 20:04:48 +0100

    Added specifier_S function body

--- specifier_S.c
+++ specifier_S.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
  * specifier_S - Function to handle the custom S specifier
@@ -7,6 +8,19 @@
  */
 void specifier_S(int *counter, void *value)
 {
-	(void)counter;
-	(void)value;
+	char *v = (char *)value;
+	int i = 0;
+
+	while (v[i])
+	{
+		if (v[i] < 32 || v[i] > 126)
+		{
+			*counter += _puts("\\x");
+			if (v[i] <= 15)
+				*counter += _putchar('0');
+			print_hex(v[i++], 0, counter);
+			continue;
+		}
+		*counter += _putchar(v[i++]);
+	}
 }
